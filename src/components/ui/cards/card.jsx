337d1b4682499a59d8b9656712608bc2f59cb266
Ann-Kiende <annkiende30@gmail.com>
2025-12-10 23:59:14 +0300

    Add UI folder for reusable components

---
+++ src/components/ui/cards/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.module.css';
+
+const card = () => {
+  return (
+    <div>
+      <button className='primary-btn'></button>
+    </div>
+  );
+};
+
+export default card;
